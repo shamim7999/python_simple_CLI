6b497135a749d2e43833f770fa08153da4141e2a
shamim7999 <10shamimsarker01@gmail.com>
2024-05-28 14:34:04 +0600

    removed CLI project.

--- CLI/models/person.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, person_id, name, salary):
-        self.id = person_id
-        self.name = name
-        self.salary = salary
-
-    def __str__(self):
-        return f"Person(id={self.id}, name='{self.name}', salary={self.salary})"
